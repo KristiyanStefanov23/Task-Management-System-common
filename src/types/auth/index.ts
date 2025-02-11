049a80f773e737d508b907cd23975d353559720c
Kristiyan Stefanov <46137735+KristiyanStefanov23@users.noreply.github.com>
2025-02-11 02:12:57 +0200

    add: more common types

--- src/types/auth/index.ts
+++ src/types/auth/index.ts
@@ -6,3 +6,16 @@ export type AccessTokenPayload = {
 }
 
 export type RefreshTokenPayload = Pick<AccessTokenPayload, 'sub' | 'iat'>
+
+
+export type LoginCredentials = {
+	email: string
+	password: string
+}
+
+export type RegisterCredentials = {
+	email: string
+	name: string
+	password: string
+	repeatPassword: string
+}
--- src/types/tasks/index.ts
+++ src/types/tasks/index.ts
@@ -9,6 +9,7 @@ export enum TaskStatus {
 export type TaskCreationAttributes = {
 	title: string
 	description: string
+	assignedUserId: string
 	status?: TaskStatus
 }
 
@@ -17,6 +18,20 @@ export type TaskAttributes = {
 	title: string
 	description: string
 	status: TaskStatus
-	assignedUserId?: string
+	assignedUserId: string
 	assignedUser?: UserAttributes
+}
+
+export type TaskAttributesWithUserName = TaskAttributes & {
+	assignedUserName: string
+}
+
+type UserTaskCount = {
+	user: string
+	count: number
+}
+
+export type TaskAnalytics = {
+	statusCounts: { [key in TaskStatus]: number }
+	userTaskCounts: UserTaskCount[]
 }
